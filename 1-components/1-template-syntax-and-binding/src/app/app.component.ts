4f75ce966e99a151923f147ef6c002131eac2c90
Dmitry Kushnir <sendmail.dima@gmai.com>
2020-06-07 22:00:22 +0300

    hw1

--- 1-components/1-template-syntax-and-binding/src/app/app.component.ts
+++ 1-components/1-template-syntax-and-binding/src/app/app.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component,  OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements AfterContentInit, OnInit {
+  public Products = {
+    _id: 'hfn57cv585',
+    name: 'Oranges',
+    description: 'Oranges are stuffed with liquid sunshine!',
+    feedbacksCount: 2,
+    images: [{
+      url: 'https://producemadesimple.ca/wp-content/uploads/2015/01/orange-web-600x450.jpg',
+      sourse: 'jf7hm8vu',
+    }],
+    price: [{
+      price: 2,
+      currency: '$',
+      amount: 1,
+      metrics: 'kg',
+    }],
+    rating: 5,
+    status: true,
+  };
+  public chosenProduct!: any;
+  public product: any;
+ // @ContentChild('terminalinfo', { static: true }) public terminalinfo!: ElementRef;
+
+  constructor() {
+  }
+
+  public addProduct($event: any) {
+    this.product = $event;       // $event :any
+    this.chosenProduct = 'name: ' + this.product.name + '\n' + 'price: ' + this.product.price[0].price +
+      this.product.price[0].currency + '/' + this.product.price[0].metrics + '\n' + 'rating: ' + this.product.rating;
+
+  }
+
+  ngAfterContentInit(): void {
+
+  }
+
+  ngOnInit(): void {
+   // console.log('terminalinfo', this.terminalinfo);
+  }
+
+
+}
--- 1-components/1-template-syntax-and-binding/src/app/card/card.component.ts
+++ 1-components/1-template-syntax-and-binding/src/app/card/card.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input,  Output } from '@angular/core';
 
 @Component({
   selector: 'app-shop-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent {}
+export class CardComponent {
+  @Input() product:any;
+  @Output() public addToCart: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() {
+
+  }
+
+
+  public addProduct() {
+this.addToCart.emit(this.product);
+  }
+}
